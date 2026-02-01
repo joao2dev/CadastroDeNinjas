1a37a73c1ab7669bb284beed47b7c8040df53855
Joao2dev <beneficil57@gmail.com>
2026-02-01 14:46:25 -0300

    Feat:adiciona a coluna de rank ao Db

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -29,6 +29,9 @@ public class NinjaModel {
     @Column(name = "idade")
     private int idade;
 
+    @Column(name = "rank")
+    private String rank;
+
     @Column(name = "img_url")
     private String img;
 
